32624103cf9f2969a801425250e786bb079e76a0
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-08-15 16:18:13 -0300

    Feed e Publicacao API

--- components/feed/index.js
+++ components/feed/index.js
@@ -9,34 +9,31 @@ export default function Feed({usuarioLogado}){
 
    useEffect (() => {
     async function  dadosPostagens() {
-      setListaDePostagens([]);
-      const { data } = await feedService.carregarPostagens();    
-        if(data.length > 0 ){
-        const postagensFormatadas = data.map((postagem) =>({ 
-        id: postagem._id,
-        usuario:{
-           id: postagem.userId,
-           nome:postagem.usuario.nome,
-           avatar:postagem.usuario.avatar 
-        },
-         fotoDoPost: postagem.foto,
-         descricao: postagem.descricao,
-         curtidas: postagem.likes,
-         comentarios: postagem.comentarios.map(c => ({
-         nome: c.nome,
-         mensagem: c.comentario
-         }))
-   })); 
-   setListaDePostagens(postagensFormatadas);
-  }      
-  
+            setListaDePostagens([]);
+            const { data } = await feedService.carregarPostagens();    
+               
+                    const postagensFormatadas = data.map((postagem) =>({ 
+                    id: postagem._id,
+                    usuario:{
+                      id: postagem.userId,
+                      nome:postagem.usuario.nome,
+                      avatar:postagem.usuario.avatar 
+                    },
+                    fotoDoPost: postagem.foto,
+                    descricao: postagem.descricao,
+                    curtidas: postagem.likes,
+                    comentarios: postagem.comentario.map(c => ({
+                      nome: c.nome,
+                      mensagem: c.comentario
+                    }))              
+              })); 
+              setListaDePostagens(postagensFormatadas);
+              console.log(postagensFormatadas);
+            
+  }       
   dadosPostagens();
-}
-},[usuarioLogado]);
-      
+},[]);
 
-
-   
       return(
         <div className="feedContainer largura30pctDesktop">
           {listaDePostagens.map(dadosPostagem =>(
@@ -45,9 +42,10 @@ export default function Feed({usuarioLogado}){
               {...dadosPostagem}
               usuarioLogado={usuarioLogado}
               />
-            ))
-          }  
+            ))}  
         </div>   
       )
+
  }
+ 
    
